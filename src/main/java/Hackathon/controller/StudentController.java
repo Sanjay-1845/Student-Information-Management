c7e5bb199f06cffdb613215563a49ef0c9d5676b
Sanjay Pratap Singh <thakursanjay12012003@gmail.com>
2023-07-14 21:16:14 +0530

    Controller code

--- src/main/java/Hackathon/controller/StudentController.java
+++ src/main/java/Hackathon/controller/StudentController.java
@@ -1,4 +1,34 @@
 package Hackathon.controller;
+import Hackathon.entity.Student;
+import Hackathon.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.browseStudent();
+    }
+
+    @PutMapping("/{studentId}")
+    public Student updateStudent(@PathVariable Long studentId, @RequestBody Student student) {
+        student.setId(studentId);
+        return studentService.updateStudent(student);
+    }
+
+    @PatchMapping("/{studentId}/deactivate")
+    public void deactivateStudent(@PathVariable Long studentId) {
+        studentService.deactivateStudent(studentId);
+    }
 }
+
